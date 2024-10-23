866977031e182a11a9ee6bf7f342705be28816d4
JuanDiego2004 <peraltaarrietajuan123@gmail.com>
2024-10-23 08:50:29 -0500

    carga inicial

---
+++ sistema-pos-java/src/main/java/com/sistema/pos/sistema/pos/java/SistemaPosJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sistema.pos.sistema.pos.java;
+
+/**
+ *
+ * @author JuanDiego
+ */
+public class SistemaPosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
